c4eda1b69fa85230e8df24dfaa57ea7a21df44e3
KaylinSoobz <kaylinsubramanian@gmail.com>
2026-09-23 11:05:26 +0200

    Cleaned up code , removed duplicate code and merged components

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,42 +1,95 @@
-import CountTasks from "../Processes/CountTasks.jsx"
-import CalculateProgress from "../Processes/CalculateProgress.jsx"
+import initialTasks from "../data/tasks.jsx"
 import DisplayPriority from "../Processes/DisplayPriority.jsx"
-import CalculateCompletedPercentage from "../Processes/CalculateCompletedPercentage.jsx"
+
+
+const colorMap = {
+    green : "bg-emerald-500",
+    orange : "bg-orange-500",
+    gray : "bg-gray-200"
+}
+
+    const TaskProcess = (props) => {
+        const count = initialTasks.reduce((tasks,task) => task.status === props.taskStatus ? tasks + 1 : tasks ,0);
+        const width = (count/initialTasks.length)*100
+        const percent = (count/initialTasks.length)*100
+        const Process = () => {
+            if (props.process === "countTasks"){
+             return (
+                 <div>
+                     {count}
+                 </div>
+             )}
+             else if (props.process === "calculateProgress"){
+             return (
+                 <div 
+                 style={{width : `${width}%`}}
+                 className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center ${colorMap[props.color]}`}
+                 />
+             )}
+            else if(props.process === "getPercentComplete"){
+             return (
+                <div className="flex">
+                     <h3 className="pl-2 mr-2">Completed</h3>
+                     <section className="text-green-500">{percent}%</section>
+                </div>
+             )}};
+             return Process();
+    }
 
 const DashBoard = () => {
     return (
         <div className="flex flex-col flex-1 h-full bg-gray-100 items-center">
-            <label className="pt-3">Tasks</label>
-            <div className="flex flex-row justify-between m-4">
-                <span className="flex flex-col items-center border border-gray-200 bg-white p-6">
-                    <CountTasks taskStatus="completed" />
-                    <label>Completed</label>
+            <div className="flex flex-row w-1/2 h-2/15 justify-around mt-6">
+                <span className="flex flex-col w-1/5 items-center justify-center border border-gray-200 bg-white p-6 hover:border-green-500">
+                    <TaskProcess
+                        process="countTasks"
+                        taskStatus="completed"
+                    />
+                    <h4>Completed</h4>
                 </span>
-                <span className="flex flex-col items-center border border-gray-200 bg-white p-6">
-                    <CountTasks taskStatus="in-progress" />
-                    <label>In Progress</label>
+                <span className="flex flex-col w-1/5 items-center justify-center border border-gray-200 bg-white p-6 hover:border-orange-500">
+                    <TaskProcess
+                        process="countTasks"
+                        taskStatus="in-progress"
+                    />
+                    <h4>In-progress</h4>
                 </span>
-                <span className="flex flex-col items-center border border-gray-200 bg-white p-6">
-                    <CountTasks taskStatus="todo" />
-                    <label>To Do</label>
+                <span className="flex flex-col w-1/5 items-center justify-center border border-gray-200 bg-white p-6 hover:border-red-500">
+                    <TaskProcess
+                        process="countTasks"
+                        taskStatus="todo"
+                    />
+                    <h4>To-do</h4>
                 </span>
             </div>
-            <div className="relative pt-1 mx-5 w-full max-w-md pt-3">
+            <div className="relative pt-1 mx-5 w-full max-w-md mt-3">
                 <div className="flex">
-                <h3 className="pl-2 mr-2">Progress</h3>
-                <CalculateCompletedPercentage/>             
+                    <TaskProcess
+                        process="getPercentComplete"
+                        taskStatus="completed"
+                    />
                 </div>
                 <div className="overflow-hidden h-4 mb-4 text-xs flex rounded bg-gray-200 border border-gray-300">
-                    <CalculateProgress taskStatus="completed" color="green"/>
-                    <CalculateProgress taskStatus="in-progress" color="orange"/>
-                    <CalculateProgress taskStatus="todo" color="gray"/>
+                    <TaskProcess
+                        process="calculateProgress"
+                        taskStatus="completed"
+                        color="green"
+                    />
+                    <TaskProcess
+                        process="calculateProgress"
+                        taskStatus="in-progress"
+                        color="orange"
+                    />
+                    <TaskProcess
+                        process="calculateProgress"
+                        taskStatus="todo"
+                        color="gray"
+                    />
                 </div>
             </div>
-
-            <DisplayPriority/>
-
+            <DisplayPriority />
         </div>
-    )
-}
+    );
+};
 
 export default DashBoard;
